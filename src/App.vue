2dbaa2d5f657cbafc623c6ac21a00b143ebd3b28
Yuanmingyangymy <106372033+Yuanmingyangymy@users.noreply.github.com>
2023-02-09 11:46:37 +0800

    报名页面
    initial try

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="3G logo" src="./assets/logo.png">
+    <PersonalInfo/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import PersonalInfo from './components/PersonalInfo'
+  export default {
+    name: 'App',
+    components: {
+      PersonalInfo
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  /* base */
+  * {
+      margin: 0;
+      padding: 0;
+      /*标准盒子 */
+      box-sizing: border-box;
+  }
+  body {
+    background-color: #3e3d3d;
+  }
+  img {
+    position: absolute;
+    left: 180px;
+    top: 10px;
+  }
+  .box {
+      width: 400px;
+      height: 400px;
+      position: relative;
+      left: 650px;
+      top: 180px;
+      padding: 15px;
+      background: #fff;
+      border-radius: 10px;
+      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),0 10px 10px rgba(0, 0, 0, 0.22);
+      text-align: center;
+  }
+  input{
+      width: 100%;
+      height: 48px;
+      text-indent: 12px;
+      border: 1px solid #ccc;
+      /* 把input上左右边框取消 */
+      border-left: none;
+      border-right: none;
+      border-top: none;
+      /* 点击input边框消失 */
+      outline: none;
+      margin: 12px 0;
+      font-size: 16px;
+  }
+  button {
+      padding: 0.4rem 1rem;
+      background: #417dff;
+      color: white;
+      border: 1px solid #fff;
+      outline: none;
+      /* 鼠标放上变小手 */
+      cursor: pointer;
+      width: 50px;
+      border-radius: 10px;
+      transition: all 30ms ease-in;
+      margin: 20px 0;
+      font-size: 14px;
+      padding: 8px 0;
+  }
+  /* 被选中时候缩小 */
+  button:active{
+      transform: scale(0.95,0.95);
+  }
+  a {
+      display: block;
+      font-size: 12px;
+      cursor: pointer;
+      color: rgb(0, 195, 255);
+      margin-bottom: 8px;
+  }
+  a:hover {
+      text-decoration: underline;
+  }
 </style>
---
+++ src/components/PersonalInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+        <form action="#" id="login" enctype="application/x-www-form-urlencoded">
+            <h2>报名</h2>
+            <input type="text" placeholder="姓名" id="username">
+            <input type="text" placeholder="电话号码" id="tel">
+            <input type="text" placeholder="QQ号" id="qq">
+            <button>OK</button>
+            <a id="a2">If you don't already have an account, click here to register</a>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PersonalInfo'
+    }
+</script>
